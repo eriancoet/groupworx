59ec123dc13a3dd5a3b060a9b4492f783d9f888d
Rian Coetzee <eriancoet@gmail.com>
2024-05-03 10:08:58 +0200

    slider update

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-
-    dropdownToggles.forEach(toggle => {
-        toggle.addEventListener('click', function(event) {
-            const clickedDropdownContent = toggle.nextElementSibling;
-            const allDropdownContents = document.querySelectorAll('.dropdown-content');
-            
-            allDropdownContents.forEach(function(dropdownContent) {
-                if (dropdownContent !== clickedDropdownContent) {
-                    dropdownContent.classList.remove('show');
-                }
-            });
-            
-            clickedDropdownContent.classList.toggle('show');
-        });
-    });
-
-    window.addEventListener('click', function(event) {
-        if (!event.target.matches('.dropdown-toggle')) {
-            const dropdowns = document.querySelectorAll('.dropdown-content');
-            dropdowns.forEach(function(dropdown) {
-                if (dropdown.classList.contains('show')) {
-                    dropdown.classList.remove('show');
-                }
-            });
-        }
-    });
-
-    // Select the hamburger menu and navigation links
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    // Add event listener to the hamburger menu
-    hamburgerMenu.addEventListener('click', () => {
-        // Toggle the 'show' class on the navigation links
-        navLinks.classList.toggle('show');
-        // Toggle the 'open' class on the hamburger menu
-        hamburgerMenu.classList.toggle('open');
-    });
-});
-
-
+function sliderFunc() {
+    let sliderBox = document.querySelector('.slider');
+    let tapToRightBtn = document.querySelector('.btn');
+    let index = 0;
+    sliderBox.style.width = '500px';
+    sliderBox.style.left = '0px';
+  
+    function sliderNext() {
+      setTimeout(() => {
+        index++;
+        let child = sliderBox.querySelector(`div:nth-child(${index})`);
+        let cloneNode = child .cloneNode(true);
+        sliderBox.style.width = `${(5 + index) * 100}px`;
+        sliderBox.appendChild(cloneNode);
+      }, 300);
+      // clone and move the element to bottom
+      let currentLeftPosition = sliderBox.style.left ? parseFloat(sliderBox.style.left.replace('px', '')) : 0;
+      let nextLeftPosition = currentLeftPosition - 100;
+      sliderBox.style.left = `${nextLeftPosition}px`;
+  
+    }
+    tapToRightBtn.addEventListener('click', sliderNext);
+  }
+  sliderFunc();
---
+++ slider.js
@@ -0,0 +1,26 @@
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+  
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+  
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
+  
